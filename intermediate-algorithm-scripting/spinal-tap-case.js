2703c9c05665c7f05bc183854c7c2cd6b6f96e87
parmeet <parmeet.asi@gmail.com>
2018-09-05 05:16:54 +0530

    completed spinal tap case algo question

--- intermediate-algorithm-scripting/spinal-tap-case.js
+++ intermediate-algorithm-scripting/spinal-tap-case.js
@@ -1,7 +1,10 @@
 function spinalCase(str) {
   // "It's such a fine line between stupid, and clever."
   // --David St. Hubbins
-  str = str.split(" ").join("-").toLowerCase();
+  let strRegex = /([a-z])([A-Z])/g;
+  str = str.replace(strRegex, "$1 $2");
+  str = str.replace(/\s|_/g, "-").toLowerCase();
+
   return str;
 }
 console.log(spinalCase("This Is Spinal Tap"));
